b79050d4af96cf0c17a46dc80441dad04d0621eb
atg <2601259226@qq.com>
2022-08-28 17:27:52 +0800

    登录模块完成 增加购物车模块

---
+++ gulimall-auth/src/test/java/com/atg/gulimall/auth/GulimallAuthApplicationTests.java
@@ -0,0 +1,19 @@
+package com.atg.gulimall.auth;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class GulimallAuthApplicationTests {
+
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
